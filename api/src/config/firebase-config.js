1b03bc43938be700b4b90d6c588b105efd1c7790
wcamest <112977706+wcamest@users.noreply.github.com>
2023-02-14 13:26:57 -0500

    Delete api/src/config directory

--- api/src/config/firebase-config.js
+++
@@ -1,8 +0,0 @@
-var firebaseAdmin = require("firebase-admin");
-var serviceAccount = require("./ServiceAccount.json");
-
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount)
-});
-
-module.exports = firebaseAdmin;
